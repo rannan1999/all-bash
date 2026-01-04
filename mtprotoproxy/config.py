09384379e88d25ba5addb688945a9d2e3200ce41
rannan1999 <rannan@mingfei1982.eu.org>
2026-01-04 09:13:50 +0800

    Add files via upload

---
+++ mtprotoproxy/config.py
@@ -0,0 +1,14 @@
+LISTEN_ADDR_IPV4 = "0.0.0.0" 
+PORT = 27000 
+
+USERS = {
+    "tg": "085fca96b482de27bc5ec034d8ddf061",
+}
+
+MODES = {
+    "classic": False,
+    "secure": False,
+    "tls": True 
+}
+
+TLS_DOMAIN = "www.microsoft.com"
